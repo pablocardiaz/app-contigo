0a28c504de93ff83ad92770c164e0157a85f3db0
Yenifer <k.dreams17@gmail.com>
2022-06-24 00:51:34 -0400

    agenda

--- src/app/pages/agenda/agenda.page.ts
+++ src/app/pages/agenda/agenda.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-agenda',
   templateUrl: './agenda.page.html',
@@ -7,9 +7,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgendaPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public alertController: AlertController
+
+  ) { }
 
   ngOnInit() {
   }
+  //Especialidad medica
+  especialidadMedica() {
+    this.alertController.create({
+      header: 'Lista de especialidades',
+      message: 'Selecciona una especialidad',
+      inputs: [
+        {
+          type: 'radio',
+          label: 'Extra Small',
+          value: 'xs'
+        },
+        {
+          type: 'radio',
+          label: 'Small',
+          value: 's'
+        },
+        {
+          type: 'radio',
+          label: 'Medium',
+          value: 'm'
+        },
+        {
+          type: 'radio',
+          label: 'Large',
+          value: 'l'
+        },
+        {
+          type: 'radio',
+          label: 'Extra Large',
+          value: 'xl'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: (data: any) => {
+            console.log('Canceled', data);
+          }
+        },
+        {
+          text: 'Aceptar',
+          handler: (data: any) => {
+            console.log('Selecciono la siguiente especialidad: ', data);
+          }
+        }
+      ]
+    }).then(res => {
+      res.present();
+    });
+  }
+
+//medico
+nombresMedicos(): void {
+  this.alertController.create({
+    header: 'Lista de Medicos',
+    message: 'Seleccione un Medico',
+    inputs: [
+      {
+        type: 'radio',
+        label: 'Extra Small',
+        value: 'xs'
+      },
+      {
+        type: 'radio',
+        label: 'Small',
+        value: 's'
+      }
+      
+    ],
+    buttons: [
+      {
+        text: 'Cancelar',
+        handler: (data: any) => {
+          console.log('Cancelar', data);
+        }
+      },
+      {
+        text: 'Aceptar',
+        handler: (data: any) => {
+          console.log('Selecciono a: ', data);
+        }
+      }
+    ]
+  }).then(res => {
+    res.present();
+  });
+}
+
+
+
 
 }
